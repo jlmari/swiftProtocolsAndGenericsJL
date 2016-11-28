eca9b83a83c30b990eef70c676f52eba5c672cfe
Juan Luis <ravelli@Air-de-Juan.oficina.visual-engin.com>
2016-11-28 12:00:04 +0100

    initial repo

---
+++ swiftProtocolsAndGenericsJL/swiftProtocolsAndGenericsJL/swiftProtocolsAndGenericsJL.h
@@ -0,0 +1,19 @@
+//
+//  swiftProtocolsAndGenericsJL.h
+//  swiftProtocolsAndGenericsJL
+//
+//  Created by Juan Luis on 28/11/16.
+//  Copyright © 2016 Juan Luis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftProtocolsAndGenericsJL.
+FOUNDATION_EXPORT double swiftProtocolsAndGenericsJLVersionNumber;
+
+//! Project version string for swiftProtocolsAndGenericsJL.
+FOUNDATION_EXPORT const unsigned char swiftProtocolsAndGenericsJLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftProtocolsAndGenericsJL/PublicHeader.h>
+
+
